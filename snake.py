ac7f7d736aab1e0c0bbd9e73b1f0d8e0cfc35425
Kip <kip.riecken@gmail.com>
2026-01-26 20:40:20 -0500

    snake moving freely

--- snake.py
+++ snake.py
@@ -1,33 +1,49 @@
 import curses
-from curses import wrapper, cbreak
+from curses import wrapper
+
 
 def main(stdscr):
+    direction = 'DOWN'
     curses.curs_set(0)
     stdscr.clear()
     position = {'x': 70, 'y': 0}
+    curses.halfdelay(1)
+    stdscr.nodelay(True)
 
     stdscr.addstr(position['y'], position['x'], "#")
     stdscr.refresh()
     curses.noecho()
 
     stdscr.keypad(True)
+    counter = 0
 
-    cbreak()
     while True:
+        counter += 1
         val = stdscr.getch()
+        if val == -1:
+            val = None
         match val:
             case curses.KEY_RIGHT:
-                position['x'] += 1
+                direction = 'RIGHT'
             case curses.KEY_UP:
-                if position['y'] > 0:
-                    position['y'] -= 1
+                direction = 'UP'
             case curses.KEY_DOWN:
-                position['y'] += 1
+                direction = 'DOWN'
             case curses.KEY_LEFT:
+                direction = 'LEFT'
+            case 'q' | 'Q':
+                break
+        match direction:
+            case 'RIGHT':
+                position['x'] += 1
+            case 'LEFT':
                 if position['x'] > 0:
                     position['x'] -= 1
-            case ord('q'):
-                break
+            case 'UP':
+                if position['y'] > 0:
+                    position['y'] -= 1
+            case 'DOWN':
+                position['y'] += 1
         stdscr.clear()
         stdscr.addstr(position['y'], position['x'], "#")
         stdscr.refresh()
