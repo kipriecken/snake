488825b7d2da1520b6b81485f70027f2e242dd29
Kip <kip.riecken@gmail.com>
2026-01-27 23:17:02 -0500

    change body to list of tuples (make actual snake), end game if out of bounds

--- snake.py
+++ snake.py
@@ -6,22 +6,28 @@ def main(stdscr):
     direction = 'DOWN'
     curses.curs_set(0)
     stdscr.clear()
-    position = {'x': 70, 'y': 0}
+    snake = [
+        (70, 3),
+        (70, 2),
+        (70, 1),
+        (70, 0),
+    ]
     curses.halfdelay(1)
     stdscr.nodelay(True)
 
-    stdscr.addstr(position['y'], position['x'], "#")
+    for pos in snake:
+        stdscr.addstr(pos[1], pos[0], "#")
     stdscr.refresh()
     curses.noecho()
 
     stdscr.keypad(True)
-    counter = 0
 
     while True:
-        counter += 1
         val = stdscr.getch()
         if val == -1:
             val = None
+        if snake[0][0] in (0, curses.COLS - 1) or snake[0][1] in (0, curses.LINES - 1):
+            break
         match val:
             case curses.KEY_RIGHT:
                 direction = 'RIGHT'
@@ -35,17 +41,29 @@ def main(stdscr):
                 break
         match direction:
             case 'RIGHT':
-                position['x'] += 1
+                if (snake[0][0] + 1, snake[0][1]) in snake:
+                    break
+                if snake[0][0] < curses.COLS - 1:
+                    snake.insert(0, (snake[0][0] + 1, snake[0][1]))
             case 'LEFT':
-                if position['x'] > 0:
-                    position['x'] -= 1
+                if (snake[0][0] - 1, snake[0][1]) in snake:
+                    break
+                if snake[0][0] > 0:
+                    snake.insert(0, (snake[0][0] - 1, snake[0][1]))
             case 'UP':
-                if position['y'] > 0:
-                    position['y'] -= 1
+                if (snake[0][0], snake[0][1] - 1) in snake:
+                    break
+                if snake[0][1] > 0:
+                    snake.insert(0, (snake[0][0], snake[0][1] - 1))
             case 'DOWN':
-                position['y'] += 1
+                if (snake[0][0], snake[0][1] + 1) in snake:
+                    break
+                if snake[0][1] < curses.LINES - 1:
+                    snake.insert(0, (snake[0][0], snake[0][1] + 1))
+        snake.pop()
         stdscr.clear()
-        stdscr.addstr(position['y'], position['x'], "#")
+        for pos in snake:
+            stdscr.addstr(pos[1], pos[0], "#")
         stdscr.refresh()
 
     stdscr.keypad(False)
